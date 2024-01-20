646bc253575d3e7071749d4a71f67e486c3a5dce
Vasjon Done <donevasjon@gmail.com>
2024-01-20 17:06:32 +0100

    fix(invoices): remove throw exeption from delete

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -71,8 +71,6 @@ export async function updateInvoice(id: string, formData: FormData) {
 }
 
 export async function deleteInvoice(id: string) {
-  throw new Error('Failed to delete invoice');
-
   try {
     await sql`DELETE FROM invoices WHERE id = ${id}`;
   } catch (e) {
